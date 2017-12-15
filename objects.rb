e2f260cdf79e292e9a8164067be39a578655f9f6
James Briggs <james@bzode.com>
2017-12-15 11:52:52 -0700

    update features

--- objects.rb
+++ objects.rb
@@ -1,5 +1,7 @@
+require 'pry'
+
 @board = {
-	a: [false, false, true, false, false, false, false, false, false, false],
+	a: [false, false, false, false, false, false, false, false, false, false],
 	b: [false, false, false, false, false, false, false, false, false, false],
 	c: [false, false, false, false, false, false, false, false, false, false],
 	d: [false, false, false, false, false, false, false, false, false, false],
@@ -10,6 +12,9 @@
 	i: [false, false, false, false, false, false, false, false, false, false],
 	j: [false, false, false, false, false, false, false, false, false, false]
 }
+
+# binding.pry
+
 def player_ship_grid
     puts '   1 2 3 4 5 6 7 8 9 10'
     
@@ -29,14 +34,54 @@ def player_ship_grid
     
 end
 
-def place_ship
-    puts "Where would you like to attack?"
-    ship1 = gets.chomp!.strip
-    s_row = ship1[0]
-    col = ship1[1]
+@carrier = 5
+@battleship = 4
+@cruiser = 3
+@patrol = 2
+@sub = 3
 
-    puts s_row
-    puts col
+def place_ship
+
+    puts "Where would you like to place your ship?"
+    ship1 = gets.chomp!.strip.downcase
+    s_row = ship1[0]
+    col = (ship1[1]).to_i
+
+    puts @board[:"#{s_row}"][col]
+
+    puts ""
 end
 
-puts @board.select {|k,v| }
+
+@carrier
+def place_carrier (5)
+
+    puts "Which direction would you like to place your carrier?\n
+    'l' for left, 'r' for right 'u' for up and 'd' for down"
+
+    while direction = gets.chomp!.downcase
+        case direction
+            when 'l'
+                puts "Goes left"
+                break
+            when 'r'
+                puts "goes right"
+                break
+            when 'u'
+                puts "goes up"
+                break
+            when 'd'
+                puts "goes down"
+                break
+            else
+                puts "Invalid input please enter in 'l' 'r' 'd' or 'u'"
+        end
+    end
+end
+
+# SUDO CODE 
+# Call the ship and pass the value of ship
+# DONE # Ask which direction they would like to go. 
+# Auto incrementor in that direction for length of ship
+
+place_carrier
