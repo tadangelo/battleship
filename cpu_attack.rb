4f1a9c6d8d20f07602a743a0ec927a45240af7a9
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-12-16 00:08:40 -0600

    Randomized computer attack

--- cpu_attack.rb
+++ cpu_attack.rb
@@ -1,25 +1,30 @@
-s_row = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
-s_column = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
-@row_choice = s_row.to_a.sample
-@column_choice = s_column.to_a.sample
-@cpu_attack = @row_choice.concat @column_choice
+  # s_row = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+  # s_column = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
+  # @row_choice = s_row.to_a.sample
+  # @column_choice = s_column.to_a.sample
+  # @cpu_attack = @row_choice.concat @column_choice
 
-def computer_attack
-    @hit = 0
-       while @hit <= 16 do
-          ship1 = @cpu_attack
-          s_row = ship1[0]
-            col = (ship1[1]).to_i
-         if @board[:"#{s_row}"][col] == true
-          puts "The computer chose #{@cpu_attack}.  You got hit!"
-            @hit += 1
-            @board[:"#{s_row}"][col] = "H"
-        elsif @board[:"#{s_row}"][col] == "H"
-          puts "Opps! The computer chose #{@cpu_attack} again!"
-        else
-          puts "The computer chose #{@cpu_attack} and hit the ocean!"
-        end
-        return
-    end
 
-end
+  def computer_attack
+    s_row = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    s_column = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
+    @row_choice = s_row.to_a.sample
+    @column_choice = s_column.to_a.sample
+     @cpu_attack = @row_choice.concat @column_choice
+      @hit = 0
+      ship1 = @cpu_attack
+         while @hit <= 16 do
+            s_row = ship1[0]
+              col = (ship1[1]).to_i
+           if @board[:"#{s_row}"][col] == true
+            puts "The computer chose #{@cpu_attack}.  You got hit!"
+              @hit += 1
+              @board[:"#{s_row}"][col] = "H"
+          elsif @board[:"#{s_row}"][col] == "H"
+            puts "Opps! The computer chose #{@cpu_attack} again!"
+          else
+            puts "The computer chose #{@cpu_attack} and hit the ocean!"
+          end
+          return
+      end
+  end
