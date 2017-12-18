454dc48151f71784bce6ddcc68bddbc8fcd1dfe1
James Briggs <james@bzode.com>
2017-12-18 16:09:39 -0700

    fixed win logic

--- objects.rb
+++ objects.rb
@@ -138,6 +138,17 @@ def attack
   while @hit <= 16 || @cpu_hit <= 16
     player_attack
     computer_attack
+    
+    if @hit == 17
+        puts "Player wins!!"
+        break
+    elsif @cpu_hit == 17
+        puts "Computer kicked your butt, you must really suck because it used ZERO logic just random guessing!"
+        break
+    else
+        puts "Player has #{@hit} hits, the computer has #{@cpu_hit} hits"
+        
+    end
   end
 end
 
