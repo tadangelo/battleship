0faf3c0cb37fcf6bc841977b3ee3220be85adb75
Jason Dudley <jason.dudley27@gmail.com>
2017-12-18 11:25:19 -0700

    commit

--- objects.rb
+++ objects.rb
@@ -1,38 +1,37 @@
 require 'pry'
 
 @board = {
-	a: [false, false, false, false, false, false, false, false, false, false],
-	b: [false, false, false, false, false, false, false, false, false, false],
-	c: [false, false, false, false, false, false, false, false, false, false],
-	d: [false, false, false, false, false, false, false, false, false, false],
-	e: [false, false, false, false, false, false, false, false, false, false],
-	f: [false, false, false, false, false, false, false, false, false, false],
-	g: [false, false, false, false, false, false, false, false, false, false],
-	h: [false, false, false, false, false, false, false, false, false, false],
-	i: [false, false, false, false, false, false, false, false, false, false],
-	j: [false, false, false, false, false, false, false, false, false, false]
+  a: [false, false, false, false, false, false, false, false, false, false],
+  b: [false, false, false, false, false, false, false, false, false, false],
+  c: [false, false, false, false, false, false, false, false, false, false],
+  d: [false, false, false, false, false, false, false, false, false, false],
+  e: [false, false, false, false, false, false, false, false, false, false],
+  f: [false, false, false, false, false, false, false, false, false, false],
+  g: [false, false, false, false, false, false, false, false, false, false],
+  h: [false, false, false, false, false, false, false, false, false, false],
+  i: [false, false, false, false, false, false, false, false, false, false],
+  j: [false, false, false, false, false, false, false, false, false, false]
 }
 
 # binding.pry
 
 def player_ship_grid
-    puts '   1 2 3 4 5 6 7 8 9 10'
-    
-    @board.each do |key, row|
-        string = ''
-        row.each do |v|
-            if v
-                string += ' S'
-            elsif v == "H"
-                string += ' *'
-            else
-                string += ' .'
-            end
-        end
-    
-        puts key.to_s + ' ' + string
+  puts '   1 2 3 4 5 6 7 8 9 10'
+
+  @board.each do |key, row|
+    string = ''
+    row.each do |v|
+      if v
+        string += ' S'
+      elsif v == "H"
+        string += ' *'
+      else
+        string += ' .'
+      end
     end
 
+    puts key.to_s + ' ' + string
+  end
 end
 
 @carrier = 5
@@ -42,88 +41,88 @@ end
 @sub = 3
 
 def place_carrier
-    puts "Where would you like to place your carrier (Pick 5 spaces, 1 at a time)?"
-    5.times do
-        ship1 = gets.chomp!.strip.downcase
-        s_row = ship1[0]
-        col = (ship1[1]).to_i
+  player_ship_grid
+  puts "Where would you like to place your carrier (Pick 5 spaces, 1 at a time)?"
+  5.times do
+    ship1 = gets.chomp!.strip.downcase
+    s_row = ship1[0]
+    col = (ship1[1]).to_i
 
-        @board[:"#{s_row}"][col] = true
-    end
-    place_battleship
+    @board[:"#{s_row}"][col] = true
+  end
+  place_battleship
 end
 
 def place_battleship
-    puts "Where would you like to place your battleship (Pick 4 spaces, 1 at a time)?"
-    4.times do
-        ship1 = gets.chomp!.strip.downcase
-        s_row = ship1[0]
-        col = (ship1[1]).to_i
+  puts "Where would you like to place your battleship (Pick 4 spaces, 1 at a time)?"
+  4.times do
+    ship1 = gets.chomp!.strip.downcase
+    s_row = ship1[0]
+    col = (ship1[1]).to_i
 
-        @board[:"#{s_row}"][col] = true
-    end
-    place_cruiser
+    @board[:"#{s_row}"][col] = true
+  end
+  place_cruiser
 end
 
 def place_cruiser
-    puts "Where would you like to place your Cruiser (Pick 3 spaces, 1 at a time)?"
-    3.times do
-        ship1 = gets.chomp!.strip.downcase
-        s_row = ship1[0]
-        col = (ship1[1]).to_i
+  puts "Where would you like to place your Cruiser (Pick 3 spaces, 1 at a time)?"
+  3.times do
+    ship1 = gets.chomp!.strip.downcase
+    s_row = ship1[0]
+    col = (ship1[1]).to_i
 
-        @board[:"#{s_row}"][col] = true
-    end
-    place_sub
+    @board[:"#{s_row}"][col] = true
+  end
+  place_sub
 end
 
 def place_sub
-    puts "Where would you like to place your sub (Pick 3 spaces, 1 at a time)?"
-    3.times do
-        ship1 = gets.chomp!.strip.downcase
-        s_row = ship1[0]
-        col = (ship1[1]).to_i
+  puts "Where would you like to place your sub (Pick 3 spaces, 1 at a time)?"
+  3.times do
+    ship1 = gets.chomp!.strip.downcase
+    s_row = ship1[0]
+    col = (ship1[1]).to_i
 
-        @board[:"#{s_row}"][col] = true
-    end
-    place_patrol
+    @board[:"#{s_row}"][col] = true
+  end
+  place_patrol
 end
 
 def place_patrol
-    puts "Where would you like to place your patrol (Pick 2 spaces, 1 at a time)?"
-    2.times do
-        ship1 = gets.chomp!.strip.downcase
-        s_row = ship1[0]
-        col = (ship1[1]).to_i
+  puts "Where would you like to place your patrol (Pick 2 spaces, 1 at a time)?"
+  2.times do
+    ship1 = gets.chomp!.strip.downcase
+    s_row = ship1[0]
+    col = (ship1[1]).to_i
 
-        @board[:"#{s_row}"][col] = true
-    end
-    player_ship_grid
-    attack
+    @board[:"#{s_row}"][col] = true
+  end
+  player_ship_grid
+  attack
 end
 
 def attack
-    @hit = 0
+  @hit = 0
 
-    while @hit <= 16 do
-            puts "Where would you like to attack?"
-            ship1 = gets.chomp!.strip.downcase
-            s_row = ship1[0]
-            col = (ship1[1]).to_i
+  while @hit <= 16
+    puts "Where would you like to attack?"
+    ship1 = gets.chomp!.strip.downcase
+    s_row = ship1[0]
+    col = (ship1[1]).to_i
 
-        if @cpuboard[:"#{s_row}"][col] == true
-            puts "You got a hit!"
-            @hit += 1
-            @board[:"#{s_row}"][col] = "H"
-        elsif @board[:"#{s_row}"][col] == "H"
-            puts "Opps! You already guessed there!"
-        else
-            puts "Sorry but you hit the ocean!"
-        end
+    if @cpuboard[:"#{s_row}"][col] == true
+      puts "You got a hit!"
+      @hit += 1
+      @board[:"#{s_row}"][col] = "H"
+    elsif @board[:"#{s_row}"][col] == "H"
+      puts "Opps! You already guessed there!"
+    else
+      puts "Sorry but you hit the ocean!"
     end
+  end
 
-    puts "You won the game!!"
-
+  puts "You won the game!!"
 end
 
 # def place_carrier
@@ -150,9 +149,9 @@ end
 #         end
 #     end
 # end
-# SUDO CODE 
+# SUDO CODE
 # Call the ship and pass the value of ship
-# DONE # Ask which direction they would like to go. 
+# DONE # Ask which direction they would like to go.
 # Auto incrementor in that direction for length of ship
 
 place_carrier
