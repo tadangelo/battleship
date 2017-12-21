13872b98b524fae73d8dbbfd90de9a9705589844
Jason Dudley <jason.dudley27@gmail.com>
2017-12-20 17:35:46 -0700

    rendered computer attack on player board

--- cpu_attack.rb
+++ cpu_attack.rb
@@ -15,14 +15,28 @@ def computer_attack
     s_row = ship1[0]
     col = (ship1[1..2]).to_i - 1
     if @board[:"#{s_row}"][col] == true
+       puts "
+           _.-._
+          ({  ` )
+           ` |''   *BOOM!*
+            \_/
+     .       |       ,
+    `  -----' `-----  '
+            "
+      sleep 1
       puts "The computer chose #{@cpu_attack}.  The computer got a hit!"
       @cpu_hit += 1
       @board[:"#{s_row}"][col] = "H"
     elsif @board[:"#{s_row}"][col] == "H"
       puts "Opps! The computer chose #{@cpu_attack} again!"
     else
+      @board[:"#{s_row}"][col] == false
+      @board[:"#{s_row}"][col] = "w"
       puts "The computer chose #{@cpu_attack} and hit the ocean!"
+      # binding.pry
     end
+    computer_attack_grid
     return
   end
+  
 end
